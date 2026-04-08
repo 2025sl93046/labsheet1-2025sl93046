8961d4b75c73e06c1834b03af579460a3bf55804
2025sl93046 <2025sl93046@wilp.bits-pilani.ac.in>
2026-04-08 14:29:46 +0530

    fixed test and code

--- test_calculator.py
+++ test_calculator.py
@@ -4,13 +4,13 @@ def test_add():
     assert calculator.add(2, 3) == 5
 
 def test_sub():
-    assert calculator.sub(5, 3) == 2
+    assert calculator.subtract(5, 3) == 2
 
 def test_mul():
-    assert calculator.mul(2, 3) == 6
+    assert calculator.multiply(2, 3) == 6
 
 def test_div():
-    assert calculator.div(6, 3) == 2
+    assert calculator.divide(6, 3) == 2
 
 test_add()
 test_sub()
