d844b88c01c541a8095d9ea342ca68aa3faa870a
2025sl93046 <2025sl93046@wilp.bits-pilani.ac.in>
2026-04-08 13:39:46 +0530

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+import calculator
+
+def test_add():
+    assert calculator.add(2, 3) == 5
+
+def test_sub():
+    assert calculator.sub(5, 3) == 2
+
+def test_mul():
+    assert calculator.mul(2, 3) == 6
+
+def test_div():
+    assert calculator.div(6, 3) == 2
